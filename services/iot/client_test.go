94549355627ff51ba491e329ccad0e2ee2f3e1e4
ludanfeng <ludanfeng@zj.tech>
2020-12-18 15:43:02 +0800

    [iot] add iot client for iot core service

---
+++ services/iot/client_test.go
@@ -0,0 +1,87 @@
+package iot
+
+import (
+	"encoding/json"
+	"os"
+	"reflect"
+	"runtime"
+	"strings"
+	"testing"
+
+	"github.com/baidubce/bce-sdk-go/util/log"
+)
+
+var (
+	client *Client
+	config *Conf
+)
+
+// For security reason, ak/sk should not hard write here.
+type Conf struct {
+	AK         string `json:"ak"`
+	SK         string `json:"sk"`
+	CoreId     string `json:"iotCoreId"`
+	TemplateId string `json:"templateId"`
+}
+
+func init() {
+	log.SetLogHandler(log.STDERR)
+	log.SetLogLevel(log.DEBUG)
+
+	configJson := "config.json"
+	fp, err := os.Open(configJson)
+	if err != nil {
+		log.Fatal("config json file of ak/sk not given:", configJson)
+		os.Exit(1)
+	}
+
+	config = &Conf{}
+	decoder := json.NewDecoder(fp)
+	decoder.Decode(config)
+
+	log.Debugln(config)
+	client, _ = NewClient(config.AK, config.SK)
+}
+
+// ExpectEqual is the helper function for test each case
+func ExpectEqual(alert func(format string, args ...interface{}),
+	expected interface{}, actual interface{}) bool {
+	expectedValue, actualValue := reflect.ValueOf(expected), reflect.ValueOf(actual)
+	equal := false
+	switch {
+	case expected == nil && actual == nil:
+		return true
+	case expected != nil && actual == nil:
+		equal = expectedValue.IsNil()
+	case expected == nil && actual != nil:
+		equal = actualValue.IsNil()
+	default:
+		if actualType := reflect.TypeOf(actual); actualType != nil {
+			if expectedValue.IsValid() && expectedValue.Type().ConvertibleTo(actualType) {
+				equal = reflect.DeepEqual(expectedValue.Convert(actualType).Interface(), actual)
+			}
+		}
+	}
+	if !equal {
+		_, file, line, _ := runtime.Caller(1)
+		alert("%s:%d: missmatch, expect %v but %v", file, line, expected, actual)
+		return false
+	}
+	return true
+}
+
+func TestClientDevice(t *testing.T) {
+	err := client.DeleteDevice(config.CoreId, t.Name())
+	ExpectEqual(t.Errorf, nil, err)
+
+	result, err := client.CreateDevice(config.CoreId, config.TemplateId, t.Name(), SIGNATURE, "iot core test")
+	ExpectEqual(t.Errorf, nil, err)
+	ExpectEqual(t.Errorf, t.Name(), result.Name)
+
+	result, err = client.CreateDevice(config.CoreId, config.TemplateId, t.Name(), SIGNATURE, "iot core test")
+	ExpectEqual(t.Errorf, true, strings.Contains(err.Error(), "Device name has been occupied"))
+	ExpectEqual(t.Errorf, nil, result)
+
+	err = client.DeleteDevice(config.CoreId, t.Name())
+	ExpectEqual(t.Errorf, nil, err)
+}
